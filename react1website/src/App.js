2c62e0b91488cef97da08023a49e68378801f392
ValorWind1 <skyjdani@gmail.com>
2019-11-11 21:05:59 -0500

    first course

---
+++ react1website/src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './App.css';
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+
+class App extends React.Component {
+state = {
+todos: [
+
+  {
+    id: 1,
+    title: 'Take out the trash',
+    completed: false 
+
+  },
+  {
+    id: 2,
+    title: 'Cook dinner',
+    completed: true
+
+  },
+  {
+    id: 3,
+    title: 'Meeting with boss',
+    completed: false 
+
+  },
+  
+]
+
+}
+
+// Toggle Complete 
+markComplete = (id) => {
+  this.setState({todos: this.state.todos.map(todo => {
+    if(todo.id === id){
+      todo.completed = !todo.completed
+    }
+    return todo;
+  }) });
+}
+
+// Delete Todo
+delTodo = (id) => {
+  this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+}
+
+
+render () {
+ console.log(this.props.todos)
+
+
+  return (
+      <div className="App">
+        <Header />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      </div>
+  );
+ }
+}
+
+export default App;
